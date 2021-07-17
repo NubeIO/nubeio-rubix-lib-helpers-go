5c2329ca0532ce599d8da99a2e3acbc585210dd0
NubeDev <aidan@envrioservices.com.au>
2021-07-17 10:48:56 +1000

    setup of repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NubeIO/nubeio-rubix-lib-helpers-go
+
+go 1.16
---
+++ helpers/logs/logs.go
@@ -0,0 +1,17 @@
+package logs
+
+import (
+	"io/ioutil"
+	"log"
+)
+
+func DisableLogging(enable bool) {
+
+	if enable {
+		log.Print("INIT APP: LOGGING IS DISABLED")
+		log.SetOutput(ioutil.Discard)
+	}
+
+}
+
+
