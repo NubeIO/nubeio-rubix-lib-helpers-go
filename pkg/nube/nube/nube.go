31eb0d110e74e3b74965afe47eaf08fea690d012
Aidan P <aidan@enviroservices.com.au>
2022-04-28 08:47:10 +1000

    update nube services

--- pkg/nube/nube/nube.go
+++ pkg/nube/nube/nube.go
@@ -5,7 +5,7 @@ type service struct {
 	Proxy       string //rubix-service proxy
 	Port        int    //1717
 	PingPath    string //each service ping path
-	SystemDName string //systemctl name
+	ServiceName string //systemctl name
 }
 
 var Services = struct {
@@ -14,10 +14,12 @@ var Services = struct {
 	FlowFramework service
 	RubixService  service
 	RubixBios     service
+	PigPIO        service
 }{
-	Mosquitto:     service{Name: "mosquitto", Proxy: "", Port: 1883, SystemDName: "mosquitto"},
+	Mosquitto:     service{Name: "mosquitto", Proxy: "", Port: 1883, ServiceName: "mosquitto"},
 	BacnetServer:  service{Name: "bacnet-server", Proxy: "bacnet", Port: 1717, PingPath: "/api/system/ping"},
 	FlowFramework: service{Name: "flow-framework", Proxy: "ff", Port: 1660, PingPath: "/api/system/ping"},
 	RubixService:  service{Name: "rubix-service", Proxy: "", Port: 1616},
 	RubixBios:     service{Name: "rubix-bios", Proxy: "", Port: 1615},
+	PigPIO:        service{Name: "pigpio", Proxy: "", Port: 8888, ServiceName: "pigpiod"},
 }
